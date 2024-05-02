4b5db70d5371ff4fd66d3655996598f60db7bd6e
Flori <floriandollani15@gmail.com>
2024-05-02 11:42:39 +0200

    feat(assets): elhamdulilah added font,svg,images :bento:

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,6 +3,16 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 import { Footer } from "~/components/footer/footer";
 import { Navigation } from "~/components/navigation/navigation";
 
+import "../../public/fonts/island-moments_5.0.20/webfonts/island-moments-latin-400-normal.woff";
+import "../../public/fonts/sniglet_5.0.13/webfonts/sniglet-latin-400-normal.woff";
+import "../../public/fonts/inter_5.0.18/webfonts/inter-latin-400-normal.woff";
+import "../../public/fonts/inter_5.0.18/webfonts/inter-latin-500-normal.woff";
+import "../../public/fonts/inter_5.0.18/webfonts/inter-latin-600-normal.woff";
+import "../../public/fonts/poppins_5.0.14/webfonts/poppins-latin-400-normal.woff";
+import "../../public/fonts/poppins_5.0.14/webfonts/poppins-latin-500-normal.woff";
+import "../../public/fonts/poppins_5.0.14/webfonts/poppins-latin-600-normal.woff";
+import "../../public/fonts/poppins_5.0.14/webfonts/poppins-latin-700-normal.woff";
+
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.dev/docs/caching/
