93c6cdcb039a3fe32036a404cadccbcd91a8ef72
Flori <floriandollani15@gmail.com>
2024-05-05 17:35:39 +0200

    feat(style): elhamdulilah interactive-map xs :lipstick:

--- src/components/sections/interactiveMap/interactiveMap.tsx
+++ src/components/sections/interactiveMap/interactiveMap.tsx
@@ -1,24 +1,35 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 
-//import Image from '[IMAGE_PATH]?jsx';
+import styles from "./interactive-map.scss?inline";
+import { HiMapOutline } from "@qwikest/icons/heroicons";
+
+import ImageIllustration from "../../../../public/images/svg/interactive-map-image-illustration.svg?jsx";
+import MapIllustration from "../../../../public/images/svg/interactive-map-map-illustration.svg?jsx";
 
 export const InteractiveMap = component$(() => {
-  return (
-    <section>
-      {/** svg <Image style={{ width: '300px', height: '200px'}}/> */}
+  useStyles$(styles);
 
-      <div aria-label="content">
+  return (
+    <section class="interactive-map">
+      <ImageIllustration />
+
+      <div aria-label="content" class="interactive-map__content">
         <h1>Interactive Map</h1>
-        <div aria-label="Text content">
+        <div
+          aria-label="Text content"
+          class="interactive-map__content__text-button"
+        >
           <p>
             We have made it easy for you, adding an interactive map where you
             can find different places in Ksamil, also view their images!
           </p>
-          <button>Enter on the Map</button>
+          <button class="--tex-icon-button --color-secondary">
+            Enter on the Map <HiMapOutline />
+          </button>
         </div>
       </div>
 
-      {/** svg <Image style={{ width: '300px', height: '200px'}}/> */}
+      <MapIllustration />
     </section>
   );
 });
