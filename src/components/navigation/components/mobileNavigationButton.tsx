32c8fc31f008aed50fa5e65452e1f8ded241844f
Flori <floriandollani15@gmail.com>
2024-05-09 12:16:51 +0200

    feat(style): elhamdulilah mobile navigation :lipstick:

--- src/components/navigation/components/mobileNavigationButton.tsx
+++ src/components/navigation/components/mobileNavigationButton.tsx
@@ -1,10 +1,22 @@
-import { component$ } from "@builder.io/qwik";
-import { FlChartBars3FromLeftOutline } from "@qwikest/icons/flowbite";
+import { type Signal, component$ } from "@builder.io/qwik";
+import {
+  FlChartBars3FromLeftOutline,
+  FlCloseOutline,
+} from "@qwikest/icons/flowbite";
 
-export const MobileNavigationButton = component$(() => {
-  return (
-    <button class="nav-logo-lang-container__nav-button --icon-button">
-      <FlChartBars3FromLeftOutline />
-    </button>
-  );
-});
+export const MobileNavigationButton = component$(
+  ({ showNavigation }: { showNavigation: Signal<boolean> }) => {
+    return (
+      <button
+        class={`nav-logo-lang-container__nav-button --icon-button ${showNavigation.value ? "--color-secondary --rounded --md" : ""}`}
+        onClick$={() => (showNavigation.value = !showNavigation.value)}
+      >
+        {showNavigation.value ? (
+          <FlCloseOutline />
+        ) : (
+          <FlChartBars3FromLeftOutline />
+        )}
+      </button>
+    );
+  },
+);
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import { component$, useStyles$ } from "@builder.io/qwik";
+import { component$, useSignal, useStyles$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import { MobileNavigationButton } from "./components/mobileNavigationButton";
 
@@ -6,9 +6,12 @@ import styles from "./navigation.scss?inline";
 import { SupportUs } from "./support-section/supportUs";
 
 import Image from "../../../public/logo-placeholder.png?jsx";
+import MobileWave from "../../../public/images/svg/dropdown-menu-wave.svg?jsx";
 
 export const Navigation = component$(() => {
   useStyles$(styles);
+
+  const showNavigation = useSignal(false);
 
   return (
     <>
@@ -16,12 +19,15 @@ export const Navigation = component$(() => {
       <SupportUs />
       <div
         aria-label="Navigation logo and language container"
-        class="nav-logo-lang-container"
+        class={`nav-logo-lang-container ${showNavigation.value ? "--show" : "--hide"}`}
       >
         <div class="nav-logo-lang-container__logo">
           <Image style={{ width: "2rem", height: "2rem" }} />
           <h4>VisitKsamil</h4>
         </div>
+
+        {showNavigation.value && <MobileWave />}
+
         <nav>
           <ul>
             <li>
@@ -50,7 +56,7 @@ export const Navigation = component$(() => {
           <option value="it">It</option>
           <option value="tr">Tr</option>
         </select>
-        <MobileNavigationButton />
+        <MobileNavigationButton showNavigation={showNavigation} />
       </div>
     </>
   );
