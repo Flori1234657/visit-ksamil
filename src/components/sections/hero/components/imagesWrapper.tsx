6d7510ad7e9e32c8b922f1ee1c5e7c8a96bb0ee0
Florian Laptop <floriandollani15@gmail.com>
2025-01-21 16:24:52 +0100

    feat: :sparkles: hero images converted to webp and images shuffle loginc hero section

--- src/components/sections/hero/components/imagesWrapper.tsx
+++ src/components/sections/hero/components/imagesWrapper.tsx
@@ -1,19 +1,54 @@
 /* eslint-disable qwik/jsx-img */
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 
-import Image1 from "/images/pages/hero/image-card(1).jpg";
-import Image2 from "/images/pages/hero/image-card(2).jpg";
-import Image3 from "/images/pages/hero/image-card(3).jpg";
+import Image1 from "/images/pages/hero/image-card-1.jpg";
+import Image2 from "/images/pages/hero/image-card-2.jpg";
+import Image3 from "/images/pages/hero/image-card-3.jpg";
+import Image4 from "/images/pages/hero/image-card-4.jpg";
+import Image5 from "/images/pages/hero/image-card-5.jpg";
+import Image6 from "/images/pages/hero/image-card-6.jpg";
+
+const IMAGES = [Image1, Image2, Image3, Image4, Image5, Image6];
 
 export const ImagesWrapper = component$(() => {
+  const visibleImages = useSignal<string[]>(IMAGES.slice(0, 3)); // Start with the first three images
+  const currentIndex = useSignal(0); // Track the current starting index for the visible images
+
+  // Rotate images every 5 seconds
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(({ cleanup }) => {
+    const updateImages = () => {
+      currentIndex.value = (currentIndex.value + 1) % IMAGES.length;
+
+      // Calculate new visible images
+      visibleImages.value = [
+        IMAGES[currentIndex.value],
+        IMAGES[(currentIndex.value + 1) % IMAGES.length],
+        IMAGES[(currentIndex.value + 2) % IMAGES.length],
+      ];
+    };
+
+    // Update images every 5 seconds
+    const intervalId = setInterval(updateImages, 5000);
+
+    // Cleanup interval on component unmount
+    cleanup(() => clearInterval(intervalId));
+  });
+
   return (
     <div
       aria-label="Main section images container"
       class="hero-section__images-card"
     >
-      <img decoding="async" loading="lazy" src={Image1} alt="randrom" />
-      <img decoding="async" loading="lazy" src={Image2} alt="randrom" />
-      <img decoding="async" loading="lazy" src={Image3} alt="randrom" />
+      {visibleImages.value.map((src, index) => (
+        <img
+          key={index}
+          decoding="async"
+          loading="lazy"
+          src={src}
+          alt={`Random image ${index + 1}`}
+        />
+      ))}
     </div>
   );
 });
